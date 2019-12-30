87afce2820a1b27825758a8ad963afd18ad2f7f0
Melotron <melotron07@gmail.com>
2019-12-30 11:40:13 +0100

    Create rpitemp.py

---
+++ rpitemp.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Dont forget to install gpiozero import and requests
+#
+# sudo pip install gpiozero
+# sudo pip install import
+# sudo pip install requests
+#
+# The crontab that I'am using are: 
+# */5 * * * * sudo /home/pi/rpitemp.py >/dev/null 2>&1
+#
+
+from gpiozero import CPUTemperature
+import requests
+
+cpu = CPUTemperature()
+
+payload1 = {'svalue': int(cpu.temperature)}
+requests.get('http://192.168.1.101:8080/json.htm?type=command&param=udevice&idx=80', params=payload1)
